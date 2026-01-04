3350b02302a64caa38be95108e3181d4c8da7394
Aadi Jain <aadi192006jain@gmail.com>
2026-01-04 21:42:13 +0530

    Clarify DATABASE_URL with a comment db url
    Updated DATABASE_URL comment to indicate the database name.

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///../database/medical.db"
+DATABASE_URL = "sqlite:///../database/medical.db"  # emerzo.db
 
 engine = create_engine(
     DATABASE_URL,
